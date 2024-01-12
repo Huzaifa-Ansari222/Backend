7a1e1d0b54b019c79b6d5d6e68fd5044592667d1
Huzaifa-Ansari222 <huzaifaansari656@gmail.com>
2024-01-13 02:18:55 +0530

    push

--- gettingStartExpress1/index.js
+++ gettingStartExpress1/index.js
@@ -14,31 +14,80 @@
 // fs.readFile('a.txt','utf-8',callbackfn) 
 
 
+
+
+
+
+
 const express = require('express')
+var bodyParser = require('body-parser') //import body parser
+// parse application/json
 const app = express()
-const port = 3000
+const port = 3002
+
+//send input to ur req
+app.use(bodyParser.json())//add middleware to ur req flow and do somthing
+
+// this middleware function run before everything 
+//3 argument 
+function middleware1(req , res , next){
+    console.log('from insdie middleware: '+req.headers.counter);
+    // res.send('Error from inside middleware:');//if hide next() / able to response
+    next();//goes to     app.post('/sum', (req, res) => {   /  able to response
+    // res.send and next() both respond  so we cant use both at same time but can be use in if  else CSSCondition
+} 
+app.use(middleware1);//  
+
+
 
 //algo
 function calculateSum(counter){
     sum=0;
-    for (let i = 0; i < counter; i++) {
+    for (let i = 0; i <= counter; i++) {
     sum=sum+i;
 }
 return sum;
 }
 
-//req and response
-app.get('/sum', (req, res) => {
+function calculateMul(counter){
+    var answer=1;
+    for (let index = 1; index <= counter; index++){
+    answer=answer*index;       
+    }
+    return answer;
+}
 
-    var counter = req.query.counter;
+//req and response
+// app.get('/sum', (req, res) => {
+    app.post('/sum', (req, res) => { //route and callback function
+    // var counter = req.query.counter; //for query param
+    // console.log(req.body);//
+    // console.log(req.headers);
+    // var counter = req.headers.counter; //for header
+    var counter = req.body.counter;
+    
+    // if (counter < 100000){
     var calculatedSum=calculateSum(counter);// input
-    var answer = 'The sum is:'+ calculatedSum;
-    res.send(answer)
+    var calculatedMul=calculateMul(counter);
+    // var answer = 'The sum is:'+ calculatedSum;
+    // res.send(answer)
+    // }
+    // else{
+    // res.status(411).send("very large number input") //unauthorised
+    // }
+
+    var stringAnswer = "sum is "+calculatedSum +"mul is "+calculatedMul;
+    var answerObject={
+        sum:calculatedSum,
+        mul:calculatedMul,
+    };
+    res.status(200).send(answerObject);
 } )
 
-app.get('/',(req,res)=>{
-    res.send('home page')
+app.get('/mul',(req,res)=>{
+    res.send('This is the multiplication route.');
 })
+
 //listen incoming req on port
 //run infinite  
 app.listen(port, () => {
@@ -46,15 +95,28 @@ app.listen(port, () => {
 })
 
 
-function calculateSum(counter){
-    sum=0;
-    for (let i = 0; i < counter; i++) {
-    sum=sum+i;
-    
+// function calculateSum(counter){
+//     sum=0;
+//     for (let i = 0; i < counter; i ++) {
+//     sum=sum+i;
+// }
+// return sum;
+// }
 
-}
-return sum;
-}
-var calculatedSum=calculateSum(100);
-console.log(calculatedSum); 
 
+// var calculatedSum=calculateSum(counter);
+// var calculatedMul=calculatedMul(counter);
+// console.log(calculatedSum); 
+
+
+
+app.get('/huz',(req,res)=>{
+    res.send(`    
+<head>
+<title>huzPage</title>
+</head>
+<body>
+<b>huzz is king</b>
+</body>
+`)
+})
